840674ea758381ed3dad982b80dacfd05672fbba
mahima <dodmahima@gmail.com>
2025-09-12 13:31:49 +0530

    logger added

--- src/main/java/com/example/member/MemberServiceApplication.java
+++ src/main/java/com/example/member/MemberServiceApplication.java
@@ -1,5 +1,7 @@
 package com.example.member;
 
+import jakarta.annotation.PostConstruct;
+import org.slf4j.MDC;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -11,5 +13,8 @@ public class MemberServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MemberServiceApplication.class, args);
 	}
-
+	@PostConstruct
+	public void initMDC() {
+		MDC.put("service", "authenticator-service");
+	}
 }
